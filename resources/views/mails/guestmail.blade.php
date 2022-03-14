160e652ce87d95ff78d37108aa9a596671cd5573
ElperdonXV <davidemartinografico@gmail.com>
2022-03-14 18:08:59 +0100

    ok guestmail

---
+++ resources/views/mails/guestmail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>La mail è stata inviata</h1>
+    <p>Il tuo messaggio: {{ $lead->message }}</p>
+</body>
+</html>
