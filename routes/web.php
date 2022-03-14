745742e4159ea1a7fc9e561d250d777e14ab90d3
ElperdonXV <davidemartinografico@gmail.com>
2022-03-14 16:02:42 +0100

    work on scaffolding

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
+
+Auth::routes();
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('home');
+    });
+
+Route::get("{any?}", function () {
+    return view("guest.home");
+    })->where("any", ".*");
