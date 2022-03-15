722a08710e4e6236c907f50e9b10344b4488204b
ElperdonXV <davidemartinografico@gmail.com>
2022-03-15 17:15:47 +0100

    trying to do home with projects

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="container">
-      Ciao
+      <div class="row">
+        <div v-for="(project, index) in projects" :key="index" class="col">
+          <div class="card p-2">
+            <h1>{{ project.name }}</h1>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'Home',
-}
+    data() {
+      return{
+        projects : [],
+      }
+    },
+    created() {
+      this.getProjects();
+    },
+    methods: {
+    getProjects(){
+            axios.get('https://api.github.com/users/ElperdonXV/repos'
+            ).then((result)=>{
+                this.projects = result.data;
+                console.log(result.data);
+                console.log(this.projects);
+            }).catch((error)=>{
+                console.log(error);
+            });
+    }  
+  }
+}  
 </script>
 
 <style>
